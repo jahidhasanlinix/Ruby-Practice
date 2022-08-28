cf2d34a0c1a3a10d5b217f1ee376370975d0ebc2
JH <36171973+jahidhasanlinix@users.noreply.github.com>
2022-08-27 21:07:52 -0500

    Add files via upload

---
+++ mlgames.rb
@@ -0,0 +1,14 @@
+# Building a mad libs game
+
+puts "Enter your fav game name: "
+game_name = gets.chomp()
+
+puts "Enter the language you like most: "
+lang = gets.chomp()
+
+puts "Enter your nationality: "
+citizen = gets.chomp()
+
+puts ("My fav game is " + game_name)
+puts ("I love "+lang+ " a lot")
+puts ("I am from "+citizen)
